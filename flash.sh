b2a4221e8e82eb79d7638a748cd2199dcb0c7819
Ralph <ra@apathie.net>
2022-08-25 16:47:11 +0200

    fix flash.sh, to also work on other projects

--- flash.sh
+++ flash.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 if [ -z "$1" ]
   then
     echo "eg: flash.sh application.bin"
     exit
 fi
+
+set -x
+export FIRMWARE_PATH=$(dirname "$1")
 dd if=/dev/zero bs=1M count=4 of=./flash.bin
-dd if=./program/build/bootloader/bootloader.bin bs=1 count=$(stat -c%s ./program/build/bootloader/bootloader.bin) seek=$((16#1000)) conv=notrunc of=./flash.bin
-dd if=./program/build/partition_table/partition-table.bin bs=1 count=$(stat -c%s ./program/build/partition_table/partition-table.bin) seek=$((16#8000)) conv=notrunc of=./flash.bin
-dd if=$1 bs=1 count=$(stat -c%s $1) seek=$((16#10000)) conv=notrunc of=./flash.bin
+dd if="$FIRMWARE_PATH/bootloader/bootloader.bin" bs=1 count=$(stat -c%s "$FIRMWARE_PATH/bootloader/bootloader.bin") seek=$((16#1000)) conv=notrunc of=./flash.bin
+dd if="$FIRMWARE_PATH/partition_table/partition-table.bin" bs=1 count=$(stat -c%s "$FIRMWARE_PATH/partition_table/partition-table.bin") seek=$((16#8000)) conv=notrunc of=./flash.bin
+dd if=$1 bs=1 count=$(stat -c%s "$1") seek=$((16#10000)) conv=notrunc of=./flash.bin
