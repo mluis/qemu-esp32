e41406e140287217c3d49ca06e717e377e7f3ea4
Miguel Luis <mlluis.pt@gmail.com>
2023-09-15 23:56:06 +0000

    program: emulation: Cleanup emulation program

--- program/main/emulation.c
+++ program/main/emulation.c
@@ -1,4 +1,4 @@
-/* emulation example.
+/* Emulation example.
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
    Unless required by applicable law or agreed to in writing, this
@@ -6,53 +6,19 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "sdkconfig.h"
-#include "linenoise/linenoise.h"
-#include "argtable3/argtable3.h"
-#include "esp_netif.h"
-#include "esp_console.h"
-#include "esp_event.h"
-#include "esp_vfs_fat.h"
-#include "esp_wifi.h"
-#include "esp_mac.h"
-#include "esp_err.h"
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <sys/param.h>
+#include <esp_http_server.h>
 #include "esp_netif.h"
 #include "esp_eth.h"
-#include "esp_mac.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "sdmmc_cmd.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-
-#include <esp_http_server.h>
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
-/*
-#include "protocol_examples_common.h"
-*/
-#define CONFIG_EXAMPLE_ETH_SPI_MISO_GPIO CONFIG_EXAMPLE_ETH_PHY_RST_GPIO
-#define CONFIG_EXAMPLE_ETH_SPI_MOSI_GPIO CONFIG_EXAMPLE_ETH_PHY_RST_GPIO
-#define CONFIG_EXAMPLE_ETH_SPI_SCLK_GPIO CONFIG_EXAMPLE_ETH_PHY_RST_GPIO
-
-#include "freertos/croutine.h"
-
-/* A simple example that demonstrates how to create GET and POST
- * handlers for the web server.
- */
 
 static const char *TAG = "qemu-esp32";
 
 /* An HTTP GET handler */
 static esp_err_t hello_get_handler(httpd_req_t *req)
 {
-	ESP_LOGI(TAG, "HELLO HANDLER CALLED");
-
-
     char*  buf;
     size_t buf_len;
 
@@ -67,50 +33,6 @@ static esp_err_t hello_get_handler(httpd_req_t *req)
         }
         free(buf);
     }
-
-    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-2") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-2", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Test-Header-2: %s", buf);
-        }
-        free(buf);
-    }
-
-    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-1") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-1", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Test-Header-1: %s", buf);
-        }
-        free(buf);
-    }
-
-    /* Read URL query string length and allocate memory for length + 1,
-     * extra byte for null termination */
-    buf_len = httpd_req_get_url_query_len(req) + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found URL query => %s", buf);
-            char param[32];
-            /* Get value of expected key from query string */
-            if (httpd_query_key_value(buf, "query1", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query1=%s", param);
-            }
-            if (httpd_query_key_value(buf, "query3", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query3=%s", param);
-            }
-            if (httpd_query_key_value(buf, "query2", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query2=%s", param);
-            }
-        }
-        free(buf);
-    }
-
-    /* Set some custom headers */
-    httpd_resp_set_hdr(req, "Custom-Header-1", "Custom-Value-1");
-    httpd_resp_set_hdr(req, "Custom-Header-2", "Custom-Value-2");
 
     /* Send response with custom headers and body set as the
      * string passed in user context*/
@@ -134,118 +56,9 @@ static const httpd_uri_t hello = {
     .user_ctx  = "Hello World!"
 };
 
-/* An HTTP POST handler */
-static esp_err_t echo_post_handler(httpd_req_t *req)
-{
-    char buf[100];
-    int ret, remaining = req->content_len;
-
-    while (remaining > 0) {
-        /* Read the data for the request */
-        if ((ret = httpd_req_recv(req, buf,
-                        MIN(remaining, sizeof(buf)))) <= 0) {
-            if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-                /* Retry receiving if timeout occurred */
-                continue;
-            }
-            return ESP_FAIL;
-        }
-
-        /* Send back the same data */
-        httpd_resp_send_chunk(req, buf, ret);
-        remaining -= ret;
-
-        /* Log data received */
-        ESP_LOGI(TAG, "=========== RECEIVED DATA ==========");
-        ESP_LOGI(TAG, "%.*s", ret, buf);
-        ESP_LOGI(TAG, "====================================");
-    }
-
-    // End response
-    httpd_resp_send_chunk(req, NULL, 0);
-    return ESP_OK;
-}
-
-static const httpd_uri_t echo = {
-    .uri       = "/echo",
-    .method    = HTTP_POST,
-    .handler   = echo_post_handler,
-    .user_ctx  = NULL
-};
-
-/* This handler allows the custom error handling functionality to be
- * tested from client side. For that, when a PUT request 0 is sent to
- * URI /ctrl, the /hello and /echo URIs are unregistered and following
- * custom error handler http_404_error_handler() is registered.
- * Afterwards, when /hello or /echo is requested, this custom error
- * handler is invoked which, after sending an error message to client,
- * either closes the underlying socket (when requested URI is /echo)
- * or keeps it open (when requested URI is /hello). This allows the
- * client to infer if the custom error handler is functioning as expected
- * by observing the socket state.
- */
-esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
-{
-    if (strcmp("/hello", req->uri) == 0) {
-        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/hello URI is not available");
-        /* Return ESP_OK to keep underlying socket open */
-        return ESP_OK;
-    } else if (strcmp("/echo", req->uri) == 0) {
-        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/echo URI is not available");
-        /* Return ESP_FAIL to close underlying socket */
-        return ESP_FAIL;
-    }
-    /* For any other URI send 404 and close socket */
-    httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "Some 404 error message");
-    return ESP_FAIL;
-}
-
-/* An HTTP PUT handler. This demonstrates realtime
- * registration and deregistration of URI handlers
- */
-static esp_err_t ctrl_put_handler(httpd_req_t *req)
-{
-    char buf;
-    int ret;
-
-    if ((ret = httpd_req_recv(req, &buf, 1)) <= 0) {
-        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-            httpd_resp_send_408(req);
-        }
-        return ESP_FAIL;
-    }
-
-    if (buf == '0') {
-        /* URI handlers can be unregistered using the uri string */
-        ESP_LOGI(TAG, "Unregistering /hello and /echo URIs");
-        httpd_unregister_uri(req->handle, "/hello");
-        httpd_unregister_uri(req->handle, "/echo");
-        /* Register the custom error handler */
-        httpd_register_err_handler(req->handle, HTTPD_404_NOT_FOUND, http_404_error_handler);
-    }
-    else {
-        ESP_LOGI(TAG, "Registering /hello and /echo URIs");
-        httpd_register_uri_handler(req->handle, &hello);
-        httpd_register_uri_handler(req->handle, &echo);
-        /* Unregister custom error handler */
-        httpd_register_err_handler(req->handle, HTTPD_404_NOT_FOUND, NULL);
-    }
-
-    /* Respond with empty body */
-    httpd_resp_send(req, NULL, 0);
-    return ESP_OK;
-}
-
-static const httpd_uri_t ctrl = {
-    .uri       = "/ctrl",
-    .method    = HTTP_PUT,
-    .handler   = ctrl_put_handler,
-    .user_ctx  = NULL
-};
-
 static httpd_handle_t start_webserver(void)
 {
-    httpd_handle_t server = NULL;
+    httpd_handle_t server;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.lru_purge_enable = true;
 
@@ -255,8 +68,6 @@ static httpd_handle_t start_webserver(void)
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &hello);
-        httpd_register_uri_handler(server, &echo);
-        httpd_register_uri_handler(server, &ctrl);
         return server;
     }
 
@@ -264,41 +75,22 @@ static httpd_handle_t start_webserver(void)
     return NULL;
 }
 
-static void initialize_nvs(void)
-{
-    esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(err);
-}
-
-
-static bool started = false;
-static EventGroupHandle_t eth_event_group;
-static const int GOTIP_BIT = BIT0;
-static esp_netif_ip_info_t ip;
 static esp_eth_handle_t eth_handle = NULL;
 static esp_netif_t *eth_netif = NULL;
 
 static void event_handler(void *arg, esp_event_base_t event_base,
                           int32_t event_id, void *event_data)
 {
-    if (event_base == ETH_EVENT && event_id == ETHERNET_EVENT_START) {
-        started = true;
-    } else if (event_base == ETH_EVENT && event_id == ETHERNET_EVENT_STOP) {
-        xEventGroupClearBits(eth_event_group, GOTIP_BIT);
-        started = false;
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_ETH_GOT_IP) {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
-        memcpy(&ip, &event->ip_info, sizeof(ip));
-        xEventGroupSetBits(eth_event_group, GOTIP_BIT);
+    if (event_base == ETH_EVENT) {
+        if (event_id == ETHERNET_EVENT_START) return;
+        if (event_id == ETHERNET_EVENT_STOP) return;
     }
+    if (event_base == IP_EVENT)
+        if (event_id == IP_EVENT_ETH_GOT_IP)
+		return;
 }
 void register_ethernet(void)
 {
-    eth_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
@@ -319,26 +111,16 @@ void register_ethernet(void)
 
 }
 
-const int CONNECTED_BIT = BIT0;
-
 void app_main(void)
 {
-    uint8_t mac_addr[8] = {0x02, 0x00, 0x00, 0xBE, 0xEE, 0xEF};
-    esp_base_mac_addr_set(mac_addr);
-
-    initialize_nvs();
-
     /* Initialize ethernet dp83848 / openeth for use in qemu */
     register_ethernet();
 
-    static httpd_handle_t server = NULL;
     /* Start the server for the first time */
-    server = start_webserver();
-
+    start_webserver();
 
     printf("\n =======================================================\n");
     printf(" |               HTTP SERVER ON 80                     |\n");
     printf(" =======================================================\n\n");
-
 }
 
